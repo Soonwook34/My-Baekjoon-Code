30cdc3063c170ba18e265c89e15f580603bf7e62
박순욱(SOONWOOK PARK) <soonwook34@gmail.com>
2020-02-03 12:34:58 +0900

    Create 1330.c

---
+++ 1330.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if(a < b)
+        printf("<\n");
+    else if(a > b)
+        printf(">\n");
+    else
+        printf("==\n");
+    
+    return 0;
+}
