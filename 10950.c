a5a7b535fbd307d66cf9defa15e7e3e861434229
박순욱(SOONWOOK PARK) <soonwook34@gmail.com>
2020-02-03 12:37:04 +0900

    Create 10950.c

---
+++ 10950.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int t, a, b, i;
+    scanf("%d", &t);
+    
+    for(i=0; i<t; i++) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+    
+    return 0;
+}
