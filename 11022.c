475d69f07858b81d7de512c590997220d5639eed
박순욱(SOONWOOK PARK) <soonwook34@gmail.com>
2020-02-03 12:39:18 +0900

    Create 11022.c

---
+++ 11022.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int t, a, b, i;
+    scanf("%d", &t);
+    
+    for(i=0; i<t; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i+1, a, b, a+b);
+    }
+    
+    return 0;
+}
