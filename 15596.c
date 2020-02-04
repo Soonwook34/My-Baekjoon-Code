73f35c6f8bd73ef60a9ba4f0c44a5acf31355924
박순욱(SOONWOOK PARK) <soonwook34@gmail.com>
2020-02-04 13:57:04 +0900

    Create 15596.c

---
+++ 15596.c
@@ -0,0 +1,13 @@
+long long sum(int *a, int n) {
+	long long ans = 0;
+    
+    for(int i=0; i<n; i++)
+        ans += *(a+i);
+	return ans;
+}
+/*
+C, C11, C (Clang), C11 (Clang): long long sum(int *a, int n);
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+n: 합을 구해야 하는 정수의 개수
+리턴값: a에 포함되어 있는 정수 n개의 합
+*/
