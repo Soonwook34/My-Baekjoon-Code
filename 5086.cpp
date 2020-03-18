671d094bea3d8bf2dda585dcb4cc105d8027c7e2
박순욱(SOONWOOK PARK) <soonwook34@gmail.com>
2020-03-18 23:02:18 +0900

    Create 5086.cpp

---
+++ 5086.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    while(a != 0 && b != 0) {
+        if(a % b == 0)
+            printf("multiple\n");
+        else if(b % a == 0)
+            printf("factor\n");
+        else
+            printf("neither\n");
+        scanf("%d %d", &a, &b);
+    }
+    
+    return 0;
+}
