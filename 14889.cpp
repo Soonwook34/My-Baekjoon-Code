ed1ad0a15654dd6b5a6e0a1ba01e8f135d60fc78
박순욱(SOONWOOK PARK) <soonwook34@gmail.com>
2020-06-12 18:00:57 +0900

    Update 14889.cpp

--- 14889.cpp
+++ 14889.cpp
@@ -1,64 +1,65 @@
 #include <iostream>
-#include <climits>
+#include <algorithm>
 #include <cmath>
 using namespace std;
 
 int N;
-int minimum = INT_MAX;
-int stat[20][20];
-int startTeam[10]={0, }, linkTeam[10];
-
-void getStat(int);
+int info[21][21] = { 0, };
+int start[10] = { 0, };
+int link[10] = { 0, };
+int minV = 200000;
+void solve(int);
 
 int main() {
-    scanf("%d", &N);
-    
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            scanf("%d", &stat[i][j]);
-        }
-    }
-    
-    getStat(0);
-    printf("%d\n", minimum);
-    
-    return 0;
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			scanf("%d", &info[i][j]);
+		}
+	}
+
+	solve(0);
+
+	printf("%d\n", minV);
+
+	return 0;
 }
 
-void getStat(int step) {
-    if(step == N/2) {
-        int startStat=0, linkStat=0;
-        int cnt=0;
-        bool isInStart = false;
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N/2; j++) {
-            	if(i == startTeam[j]) {
-            		isInStart = true;
-            		break;
-            	}
-            }
-            if(!isInStart) {
-                linkTeam[cnt] = i;
-                cnt++;
-            }
-            isInStart = false;
-        }
-        
-        for(int i=0; i<N/2; i++) {
-            for(int j=0; j<N/2; j++) {
-                startStat += stat[startTeam[i]][startTeam[j]];
-                linkStat += stat[linkTeam[i]][linkTeam[j]];
-            }
-        }
-        if(abs(startStat-linkStat) < minimum)
-            minimum = abs(startStat-linkStat);
-        return;
-    }
-    
-    for(int i=0; i<N; i++) {
-        if(step==0 || i>startTeam[step-1]) {
-            startTeam[step] = i;
-            getStat(step + 1);
-        }
-    }
+void solve(int cnt) {
+	if (cnt == N / 2) {
+		bool isInStart = false;
+		int num = 0;
+		for (int i = 1; i <= N; i++) {
+			isInStart = false;
+			for (int j = 0; j < cnt; j++) {
+				if (start[j] == i) {
+					isInStart = true;
+					break;
+				}
+			}
+			if (!isInStart) {
+				link[num] = i;
+				num++;
+			}
+		}
+		int startS = 0;
+		int linkS = 0;
+		for (int i = 0; i < N / 2; i++) {
+			for (int j = 0; j < N / 2; j++) {
+				startS += info[start[i]][start[j]];
+				linkS += info[link[i]][link[j]];
+			}
+		}
+
+		minV = min(minV, abs(startS - linkS));
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (cnt == 0 || i > start[cnt - 1]) {
+			start[cnt] = i;
+			solve(cnt + 1);
+		}
+	}
 }
